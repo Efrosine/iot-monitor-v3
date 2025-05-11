37749ce400f67b3b9fd02c9cd0e00d624065d305
Erosine <efrosinegames@gmail.com>
2025-05-11 16:07:29 +0700

    add initail broadcast

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Events\newHistoryEvent;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/history', function () {
+    event(new newHistoryEvent());
+    return "done";
 });
