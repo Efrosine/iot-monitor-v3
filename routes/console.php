9515af4b65c6e101c2578448b3baeb48d32daf2e
Efrosine <efrosinegames@gmail.com>
2025-05-22 23:26:49 +0700

    feat: add ToggleDevice command to toggle device status and log responses

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('device:toggle DEV014 --on')->dailyAt('10:00');
+Schedule::command('device:toggle DEV014 --off')->dailyAt('18:00');
